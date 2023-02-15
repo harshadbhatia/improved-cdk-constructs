6119927fac28248aeadfe3375eb3acb12ad0020d
Harshad <harshadbhatia2012@gmail.com>
2023-02-15 12:21:52 +1100

    fix: fix cidr range and added vpc props (#51)

--- lib/vpc/vpc.ts
+++ lib/vpc/vpc.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps, Tags } from 'aws-cdk-lib';
+import { Stack, Tags } from 'aws-cdk-lib';
 import { SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { StringListParameter, StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
@@ -10,8 +10,8 @@ export class VPCStack extends Stack {
 
   constructor(scope: Construct, id: string, props: VPCConfig) {
     super(scope, id, props);
-
     this.config = props;
+
     this.createVPC();
     this.addTags();
     this.createParams();
@@ -44,24 +44,22 @@ export class VPCStack extends Stack {
   }
 
   createParams(): void {
+
     const vpcIdParameter = new StringParameter(this, 'VPCId', {
       stringValue: this.vpc.vpcId,
       parameterName: this.config.vpcIdSSM,
       description: this.config.vpcIdSSMDescription,
     });
-
     const privateSubnetParameter = new StringListParameter(this, 'PrivateSubnetIds', {
       stringListValue: this.vpc.privateSubnets.map((subnet) => subnet.subnetId),
       parameterName: this.config.privateSubnetSSM,
       description: this.config.privateSubnetSSMDescription,
     });
-
     const publicSubnetParameter = new StringListParameter(this, 'PublicSubnetIds', {
       stringListValue: this.vpc.publicSubnets.map((subnet) => subnet.subnetId),
       parameterName: this.config.publicSubnetSSM,
       description: this.config.publicSubnetSSMDescription,
     });
-
     const isolatedSubnetSSM = new StringListParameter(this, 'IsolatedSubnetIds', {
       stringListValue: this.vpc.isolatedSubnets.map((subnet) => subnet.subnetId),
       parameterName: this.config.isolatedSubnetSSM,
@@ -73,16 +71,14 @@ export class VPCStack extends Stack {
     this.vpc.privateSubnets.forEach((subnet) => {
       Tags.of(subnet).add('kubernetes.io/role/internal-elb', '1');
     });
-
     this.vpc.publicSubnets.forEach((subnet) => {
       Tags.of(subnet).add('kubernetes.io/role/elb', '1');
     });
-
-    this.vpc.privateSubnets.forEach((subnet) => {
-      const ar: string[] = JSON.parse(this.config.kubernetesClustersToTag as unknown as string);
+    Object.entries(this.vpc.privateSubnets).forEach(([idx, subnet]) => {
+      const ar: string[] = this.config.kubernetesClustersToTag || []
       ar.forEach((cluster) => {
         Tags.of(subnet).add(`kubernetes.io/cluster/${cluster}`, 'shared');
       });
-    });
+    })
   }
 }
